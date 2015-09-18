847322bf391306d3a50fa8547e2eff348ff79f94
Gabrielle Gerlitz <gabrielle.gerlitz@gmail.com>
2015-09-18 10:18:21 -0400

    created new database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917204842) do
+ActiveRecord::Schema.define(version: 20150917193415) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
