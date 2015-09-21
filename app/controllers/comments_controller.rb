806906f26b2f40ed9b3055b37973f5b259667891
Gabrielle Gerlitz <gabrielle.gerlitz@gmail.com>
2015-09-21 08:59:02 -0400

    Added created and update timestamps to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
-        Comment.create(body: params[:comment][:body], post: @post)
+        Comment.create(body: params[:comment][:body], post: @post,)
         redirect_to @post, notice: "New Comment Created"
     end
 
